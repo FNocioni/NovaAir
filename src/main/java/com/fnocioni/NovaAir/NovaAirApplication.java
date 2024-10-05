96dd52e1474523822987ce5b7146bc711e3ef974
FNocioni <fedenocioni@hotmail.com>
2024-10-04 22:37:52 -0500

    init commit

---
+++ src/main/java/com/fnocioni/NovaAir/NovaAirApplication.java
@@ -0,0 +1,13 @@
+package com.fnocioni.NovaAir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaAirApplication.class, args);
+	}
+
+}
